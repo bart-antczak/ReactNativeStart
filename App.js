f1b5e9c64d29ff1e77f39caf4afda6c3948d29fe
Bart <antczak.bar@gmail.com>
2018-12-16 22:43:34 +0100

    Styling na elementach, zrozumienie flex-box

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +10,26 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    placeHolder: null
+  }
+
+  placeNameChangedHandler = (val) => {
+    this.setState({
+        placeHolder: val
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>{this.state.placeHolder}</Text>
+        <TextInput
+            style={styles.textInput}
+            placeholder='An awesome place'
+            value={this.state.placeHolder}
+            onChangeText={this.placeNameChangedHandler} />
       </View>
     );
   }
@@ -32,18 +38,16 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff',
   },
-  welcome: {
-    fontSize: 20,
+  textInput: {
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5
+  }
 });
